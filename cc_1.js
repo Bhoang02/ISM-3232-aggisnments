a50e2b4ac6625d25d777a274a4aa18f3ffd21d2f
Bhoang02 <brianhoang@usf.edu>
2024-05-17 16:43:57 -0400

    Add output functionality to display bill, tip, and total

--- cc_1.js
+++ cc_1.js
@@ -8,3 +8,7 @@ let tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
 
 console.log(`The bill was $${bill}, the tip was $${tip}, and the total value $${bill + tip}.`);
 
+// 2. Create and test calcTip function
+function calcTip(bill) {
+    return (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
+}
