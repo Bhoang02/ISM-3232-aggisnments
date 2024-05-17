e154e23665460ab111edfe773968e8ccae323dd7
Bhoang02 <brianhoang@usf.edu>
2024-05-17 16:44:59 -0400

    Create and test calcTip function

--- cc_1.js
+++ cc_1.js
@@ -12,3 +12,8 @@ console.log(`The bill was $${bill}, the tip was $${tip}, and the total value $${
 function calcTip(bill) {
     return (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
 }
+
+// 3. Testing the function with a bill value of $100
+let Testbill = 100 ;
+let Testtip = calcTip(bill);
+console.log(`$${Testbill + Testtip}.`);
