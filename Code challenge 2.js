70e8b601df446953f25e62100e5f70d3ee957c2d
Bhoang02 <brianhoang@usf.edu>
2024-05-17 16:49:06 -0400

    Delete Code challenge 2.js

--- Code challenge 2.js
+++
@@ -1,4 +0,0 @@
-// Initial logic for calculating tip using ternary operator
-let bill = 275; // Example bill amount
-let tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
-console.log(`Initial logic for calculating tip using ternary operator.`); // Commit message
